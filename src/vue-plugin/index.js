4e3578f685f9f4d34647dc080c9edef04800d344
guo.feng <guo.feng@neusoft.com>
2019-07-03 12:45:52 +0800

    'Wed-Jul-03-2019-12:45:52-GMT+0800-(GMT+08:00)'

--- src/vue-plugin/index.js
+++ src/vue-plugin/index.js
@@ -2,22 +2,16 @@ export default {
   install(Vue) {
     Vue.mixin({
       created: function (arg) {
-        (!this.notBindDh) && this.$parent && this.$parent.$bindDh(this);
+        (!this.notBindDh) && (!this.pDh) && (this.$parent) && (this.$parent.$bindDh(this));
       }
     });
     
     Vue.prototype.$bindDh = function(_that = null) {    
       if (this.dh) {
         this.dh.bind(_that);
-        return;
-      }
-      
-      if (this.pDh) {
+      } else if (this.pDh) {
         this.pDh.bind(_that);
-        return;
-      }
-      
-      if (this.$parent) {
+      } else if (this.$parent) {
         this.$parent.$bindDh(that);
       }
     }
